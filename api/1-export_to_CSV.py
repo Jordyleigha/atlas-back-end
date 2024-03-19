1f7c171a7901614b4b09fcd091f6e88bc55537b0
Jordyleigha <jordana@Jordanslaptop>
2024-03-19 18:26:59 -0500

    updating

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,32 +1,53 @@
 #!/usr/bin/python3
-
-''' Module for retrieving API data'''
-
-import csv
+"""script that fetches info about a given employee using an api
+and exports it in csv format
+"""
+import json
 import requests
 import sys
 
-if __name__ == '__main__':
 
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    user_ext = '/users/{}'.format(sys.argv[1])
-    todo_ext = '/todos'
-    file_name = "{}.csv".format(sys.argv[1])
+base_url = 'https://jsonplaceholder.typicode.com'
 
-    employee_response = requests.get(base_url+user_ext)
-    employee = employee_response.json()
-    todo_response = requests.get(base_url+user_ext+todo_ext)
-    todos = todo_response.json()
+if __name__ == "__main__":
 
-    file = open(file_name, 'w')
-    csv_file = csv.writer(file, quoting=csv.QUOTE_ALL)
-    count = 0
+    user_id = sys.argv[1]
 
-    for todo in todos:
+    # get user info e.g https://jsonplaceholder.typicode.com/users/1/
+    user_url = '{}/users?id={}'.format(base_url, user_id)
+    # print("user url is: {}".format(user_url))
 
-        csv_file.writerow([
-            employee['id'],
-            employee['username'],
-            todo['completed'],
-            todo['title']])
-    file.close()
+    # get info from api
+    response = requests.get(user_url)
+    # pull data from api
+    data = response.text
+    # parse the data into JSON format
+    data = json.loads(data)
+    # extract user data, in this case, username of employee
+    user_name = data[0].get('username')
+    # print("id is: {}".format(user_id))
+    # print("name is: {}".format(user_name))
+
+    # get user info about todo tasks
+    # e.g https://jsonplaceholder.typicode.com/users/1/todos
+    tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
+    # print("tasks url is: {}".format(tasks_url))
+
+    # get info from api
+    response = requests.get(tasks_url)
+    # pull data from api
+    tasks = response.text
+    # parse the data into JSON format
+    tasks = json.loads(tasks)
+
+    # build the csv
+    builder = ""
+    for task in tasks:
+        builder += '"{}","{}","{}","{}"\n'.format(
+            user_id,
+            user_name,
+            task['completed'],  # or use get method
+            task['title']
+        )
+    with open('{}.csv'.format(user_id), 'w', encoding='UTF8') as myFile:
+        myFile.write(builder)
