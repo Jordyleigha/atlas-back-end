d7700d831ecc7e13f8fc7193470efec4eb19b2b7
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2024-03-19 19:06:43 -0500

    fixing whitespace

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -6,16 +6,16 @@ Requirements:
 Records all tasks from all employees
 Format must be: {
     "USER_ID": [
-        {"username": "USERNAME", "task": "TASK_TITLE", 
+        {"username": "USERNAME", "task": "TASK_TITLE",
         "completed": TASK_COMPLETED_STATUS},
-        {"username": "USERNAME", "task": "TASK_TITLE", 
+        {"username": "USERNAME", "task": "TASK_TITLE",
         "completed": TASK_COMPLETED_STATUS},
         ...
     ],
     "USER_ID": [
-        {"username": "USERNAME", "task": "TASK_TITLE", 
+        {"username": "USERNAME", "task": "TASK_TITLE",
         "completed": TASK_COMPLETED_STATUS},
-        {"username": "USERNAME", "task": "TASK_TITLE", 
+        {"username": "USERNAME", "task": "TASK_TITLE",
         "completed": TASK_COMPLETED_STATUS},
         ...
     ]
@@ -54,5 +54,7 @@ def export_to_json():
 
     with open('todo_all_employees.json', 'w') as json_file:
         json.dump(user_tasks, json_file)
+
+
 if __name__ == "__main__":
     export_to_json()
